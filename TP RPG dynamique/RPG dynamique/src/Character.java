3945a3e4a12eed47f3ed8cba83161f4c13c858e4
Randy Charlemagne <randy.charlemagne.pro@gmail.com>
2025-02-25 16:52:04 +0100

    Suppression fichier

---
+++ TP RPG dynamique/RPG dynamique/src/Character.java
@@ -0,0 +1,20 @@
+abstract class Character {
+    private String name;
+    private int healthPoints;
+
+    public Character(String name, int healthPoints) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(int healthPoints) {
+        this.healthPoints = healthPoints;
+    }
