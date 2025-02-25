7f5cddde956ec194e33d968333b86e366027b869
Randy Charlemagne <randy.charlemagne.pro@gmail.com>
2025-02-25 16:03:25 +0100

    Suppression fichier

--- TP RPG dynamique/RPG dynamique/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.rpg_dynamique {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.rpg_dynamique to javafx.fxml;
-    exports com.example.rpg_dynamique;
-}
